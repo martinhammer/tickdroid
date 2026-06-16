8acaae6cf7b3ea817386c04c4bd88a40edf3e41c
Martin Hammer <me@martinhammer.com>
2026-06-16 13:42:15 +0200

    Fixed jvmTarget error to bump kotlin
    Signed-off-by: Martin Hammer <me@martinhammer.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -51,9 +53,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
     buildFeatures {
         compose = true
     }
@@ -66,6 +65,14 @@ android {
                 "META-INF/LGPL2.1",
             )
         }
+    }
+}
+
+kotlin {
+    // Kotlin 2.2 removed the android.kotlinOptions DSL (jvmTarget as String is now an error);
+    // configure the compiler via the top-level compilerOptions DSL instead.
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
     }
 }
 
